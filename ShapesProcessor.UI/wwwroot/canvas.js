0ecbf62c0ec22e16277be4e2dd650d84b2e0eb63
ruslanrazakov <razakov.rus@yandex.ru>
2022-12-25 10:32:36 +0300

    UI on JS and WASM started. ClearCanvas and DrawRectangle functions implemented

---
+++ ShapesProcessor.UI/wwwroot/canvas.js
@@ -0,0 +1,29 @@
+﻿var canvas;
+
+function drawPolygon(x_1, y_1, x_2, y_2, x_3, y_3) {
+    if (canvas == undefined)
+        var canvas = document.getElementById('canvasbox');
+
+    if (canvas.getContext) {
+        var objctx = canvas.getContext('2d');
+        objctx.beginPath();
+        objctx.moveTo(x_1, y_1);
+        objctx.lineTo(x_2, y_2);
+        objctx.lineTo(x_3, y_3);
+        objctx.closePath();
+        objctx.stroke();
+
+    } else {
+        alert('You need HTML5 compatible browser to see this demo.');
+    }
+}
+
+function clearCanvas() {
+    if (canvas == undefined)
+        var canvas = document.getElementById('canvasbox');
+
+    if (canvas.getContext) {
+        var objctx = canvas.getContext('2d');
+        objctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+}
